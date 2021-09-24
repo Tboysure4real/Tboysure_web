5c832eac02bad96548a0142eec1c453cae93fbe1
Tboysure <Tboysure@Tboysure-PC>
2021-09-24 14:14:46 -0700

    commi -m 'splash screen'

---
+++ fileG/ALLEH DANIEL OVA(2014235020048)/FaceRecognitionSystem/src/facerecognitionsystem/stmt.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facerecognitionsystem;
+
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author USER
+ */
+class stmt {
+
+    static ResultSet executeQuery(String select__from_test) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
